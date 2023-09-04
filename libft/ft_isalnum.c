e0f7c6309824dd32d25f20c0aeaaba54f7baea74
Constance De Voogd <cde-voog@c1r6p2.42lehavre.fr>
2023-09-05 00:02:41 +0200

    ok5

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -6,11 +6,11 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 22:44:05 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/11 22:44:14 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/09/05 00:02:21 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "lby.h"
 
 int	ft_isalnum(int c)
 {
