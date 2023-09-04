d704d54293c497034f7a199da67c1ce84f8e1d67
Constance De Voogd <cde-voog@c1r6p2.42lehavre.fr>
2023-09-05 00:06:19 +0200

    ok7

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -6,11 +6,11 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 22:46:05 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/14 20:44:43 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/09/05 00:06:00 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "lby.h"
 
 int	ft_isascii(int c)
 {
