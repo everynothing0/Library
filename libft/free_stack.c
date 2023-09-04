4fb3e66c6baa8709d1db70a4f917b94b7613eb37
Constance De Voogd <cde-voog@c1r6p2.42lehavre.fr>
2023-09-04 23:58:30 +0200

    first change

--- libft/free_stack.c
+++ libft/free_stack.c
@@ -6,12 +6,12 @@
 /*   By: cde-voog <cde-voog@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 13:58:38 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/08/26 14:01:22 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/09/04 23:57:21 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
+#include <lby.h>
 // frees each elements in a given stack and sets the stack pointer to NULL.
 
 void	free_stack(t_stack **stack)
