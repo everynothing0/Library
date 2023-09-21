3f4dcb385b711b98505913e6ed2758f39f07269c
cde-voog <nozepastabos@gmail.com>
2023-09-21 11:27:38 +0200

    ok

--- lib/ft_lstadd_back.c
+++ lib/ft_lstadd_back.c
@@ -6,11 +6,11 @@
 /*   By: cde-voog <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 05:05:52 by cde-voog          #+#    #+#             */
-/*   Updated: 2023/04/25 05:15:07 by cde-voog         ###   ########.fr       */
+/*   Updated: 2023/09/21 11:27:02 by cde-voog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "lby.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
